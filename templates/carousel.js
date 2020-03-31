6c22f4c50ebee406b521171fdbaad55727744e59
Alexandre <alexandre.inukai@gmail.com>
2020-03-31 16:53:43 +0200

    page number on lessons

--- templates/carousel.js
+++ templates/carousel.js
@@ -3,15 +3,12 @@ checkitem = function () {
     if ($("#myCarousel .carousel-inner .item:first").hasClass("active")) {
         $this.children(".left").hide();
         $this.children(".right").show();
-        console.log("first fonction checkitem")
     } else if ($("#myCarousel .carousel-inner .item:last").hasClass("active")) {
         $this.children(".right").hide();
         $this.children(".left").show();
-        console.log("last fonction checkitem")
     } else {
         $this.children(".carousel-control").show();
     }
-    console.log("test fonction checkitem")
 };
 
 checkitem();
